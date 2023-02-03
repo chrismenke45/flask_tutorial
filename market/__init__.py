b58c378effacb9d9a70882feeeaedeab59ba1b5c
chrismenke45 <chrismenke45@gmail.com>
2023-02-02 16:02:23 -0800

    project restructuring works

--- market/__init__.py
+++ market/__init__.py
@@ -1,6 +1,7 @@
-from market import app
-
-if __name__ == '__main__':
-    app.run(debug=True)
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+db = SQLAlchemy(app)
 
 from market import routes
